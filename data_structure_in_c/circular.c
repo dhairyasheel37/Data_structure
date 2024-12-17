7ddeb7d65b20974ee668707cc24045256c7c1891
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-12-18 00:30:08 +0530

    Add files via upload

---
+++ data_structure_in_c/circular.c
@@ -0,0 +1,252 @@
+#include<stdio.h>
+#include <stdlib.h>
+typedef struct circular_linlist
+{
+    int data;
+    struct circular_linlist *next;
+}node;
+
+
+
+void *create_node(int value);
+void add_first(node **head,node **tail,int value);
+void add_last(node **head,node **tail,int value);
+void at_position(node **head,node **tail,int value,int pos);
+void delete_begin(node **head,node **tail);
+void delete_end(node **head,node **tail);
+void delete_at(node **head,node **tail,int pos);
+ void print(node **head);
+
+int main(){
+    node *head = NULL;
+	node *tail = NULL;
+	int ch,value,pos;
+
+	do
+	{
+		printf("0. Exit\n");
+		printf("1.insert at begin\n");
+		printf("2.intsert at end\n");
+		printf("3.insert at postion \n");
+		printf("4.Delete at begin\n");
+		printf("5.Delete at end\n");
+		printf("6.Delete at postion \n");
+		printf("7. Print\n");
+		printf("Enter the choice :");
+		scanf("%d",&ch);
+		
+		switch(ch)
+		{
+			case 1:
+				printf("Enter the value :");
+				scanf("%d",&value);
+				add_first(&head,&tail,value);
+				break;
+			case 2:
+				printf("Enter the value :");
+				scanf("%d",&value);
+				add_last(&head,&tail ,value);
+				break;
+			case 3:
+				printf("Enter the value :");
+				scanf("%d",&value);
+				printf("Enter the position :");
+				scanf("%d",&pos);
+				at_position(&head,&tail,value,pos);
+				break;
+			
+			case 4:
+				delete_begin(&head,&tail);
+				break;
+			case 5:
+				 delete_end(&head,&tail);
+				break;
+			case 6:
+				printf("Enter the position :");
+				scanf("%d",&pos);
+				delete_at(&head,&tail,pos);
+				break;
+			case 7:
+				print(&head);	
+		}
+		
+		
+	}while(ch!=0);
+
+    
+
+    return 0;
+}
+
+void *create_node(int value)
+{
+	node *newnode = (node*)malloc(sizeof(node));
+	newnode->data = value;
+	newnode->next = NULL;
+	return newnode;
+}
+
+void add_first(node **head,node **tail,int value)
+{
+    node *newnode = create_node(value);
+    if(*head == NULL)
+    {
+        *head= *tail = newnode;
+        (*tail)->next = *head;
+
+    }
+    else{
+        newnode->next = *head;
+        (*tail)->next = newnode;
+        *head = newnode;
+
+    }
+}
+void add_last(node **head,node **tail,int value)
+{
+    node *newnode = create_node(value);
+    if(*head == NULL)
+    {
+        *head= *tail = newnode;
+        (*tail)->next = *head;
+        return;
+    }
+    else
+    {
+        newnode->next = *head;
+        (*tail)->next = newnode;
+        *tail = newnode;
+       
+    }
+}
+
+void at_position(node **head,node **tail,int value,int pos)
+{
+    if(pos<1)
+    return;
+    node *newnode = create_node(value);
+    if(*head == NULL)
+    {
+        *head= *tail = newnode;
+        (*tail)->next = *head;
+        return;
+    }
+    else if(pos == 1)
+    {
+        add_first(head,tail,value);
+        return;
+    }
+    else
+    {
+        node* trav = *head;
+        for(int i=1;i<pos-1;i++)
+        {
+            trav = trav->next;
+        }
+        if(trav->next == *head)
+        {
+            add_last(head,tail,value);
+            return;
+        }
+        newnode->next = trav->next;
+        trav->next = newnode;
+        return;
+    }
+}
+void delete_begin(node **head,node **tail)
+{
+    if(*head == NULL)
+    return;
+    else if((*head)->next == *head)
+    {
+        free(*head);
+        *head=*tail=NULL;
+    }
+    else
+    {
+        node *temp = *head; 
+        (*tail)->next = (*head)->next;
+        *head = (*head)->next;
+        free(temp);
+    }
+}
+void delete_end(node **head,node **tail)
+{
+    if(*head == NULL)
+    return;
+    else if((*head)->next == *head)
+    {
+        free(*head);
+        *head=*tail=NULL;
+    }
+    else
+    {
+        node * trav = *head;
+        while (trav->next->next != *head)
+        {
+           trav = trav->next;
+        }
+        free(*tail);
+        *tail = trav;
+        (*tail)->next = *head;
+        
+        
+        
+    }
+}
+void delete_at(node **head,node **tail,int pos)
+{
+    if(*head == NULL || pos<1)
+    return;
+    else if(*head== *tail)
+    {
+        free(*head);
+        *head=*tail=NULL;
+        return;
+    }
+    else if(pos == 1)
+    {
+        delete_begin(head,tail);
+        return;
+    }
+    else
+    {
+        node *trav = *head;
+        for (int i = 1; i < pos-1; i++)
+        {
+            trav = trav->next;
+        }
+        if(trav->next == *head)
+        {
+            delete_end(head,tail);
+            return;
+        }    
+    
+    node*temp = trav->next;
+    trav->next = temp->next;
+    if (temp == *tail)
+    {
+       *tail = trav;
+    }
+    
+    free(temp);
+
+    }
+}
+void print(node ** head)
+{
+    if (*head == NULL) 
+    {
+        printf("List is empty\n");
+        return;
+    }
+	node *trav = *head;
+	printf("List :");
+	do
+    {
+        printf(" %d",trav->data);
+		trav = trav->next;
+    } while ((trav!=*head));
+    
+	printf("\n");
+}
